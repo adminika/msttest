c7efdc7556f59e2f69359ce5d8a0016f67e92b61
Nikolay Ponomarev <ya.mr.strak-n87@yandex.ru>
2024-09-12 19:55:29 +0700

    add mini files

---
+++ scripts/main.min.js
@@ -0,0 +1,2 @@
+(()=>{console.log("Hello Welcome !!!");var o=document.querySelector(".js-mobile"),e=document.querySelector(".js-mobile__btn");e.onclick=()=>{o.classList.toggle("mobile")};console.log("JS is Work Gulp !");})();
+//# sourceMappingURL=main.js.map
